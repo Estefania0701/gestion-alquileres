8ed3cf26a7622aeebc7b23707f78c43b8af872de
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-29 10:31:58 -0500

    Agrego CrossOrigin y método POST

--- src/main/java/com/eas/demo/gestionalquileres/controladores/UsuarioControlador.java
+++ src/main/java/com/eas/demo/gestionalquileres/controladores/UsuarioControlador.java
@@ -3,21 +3,27 @@ package com.eas.demo.gestionalquileres.controladores;
 import com.eas.demo.gestionalquileres.modelos.Usuario;
 import com.eas.demo.gestionalquileres.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioControlador {
 
     @Autowired
     private UsuarioRepositorio repositorio;
 
+    // Obtiene el listado de todos los usuarios de la BBDD
     @GetMapping("/usuarios")
-    public List<Usuario> listarTodosLosUsuarios() {
+    public List<Usuario> consultarUsuarios() {
         return repositorio.findAll();
+    }
+
+    // Crea un nuevo usuario en la BBDD
+    @PostMapping("/usuarios")
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        return repositorio.save(usuario);
     }
 }
