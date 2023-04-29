ca07cc87d0e1f0cad7c412f5daae39075064a859
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-29 11:51:02 -0500

    Agrego método para solicitud DELETE

--- src/main/java/com/eas/demo/gestionalquileres/controladores/UsuarioControlador.java
+++ src/main/java/com/eas/demo/gestionalquileres/controladores/UsuarioControlador.java
@@ -1,11 +1,15 @@
 package com.eas.demo.gestionalquileres.controladores;
 
+import com.eas.demo.gestionalquileres.excepciones.ResourceNotFoundException;
 import com.eas.demo.gestionalquileres.modelos.Usuario;
 import com.eas.demo.gestionalquileres.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,5 +29,26 @@ public class UsuarioControlador {
     @PostMapping("/usuarios")
     public Usuario crearUsuario(@RequestBody Usuario usuario) {
         return repositorio.save(usuario);
+    }
+
+    // Elimina un usuari de la BBDD
+    @DeleteMapping("/usuarios/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarUsuario (@PathVariable Long id) {
+        // si el usuario no existe, levanto la excepción personalizada
+        Usuario usuario = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID : " + id));
+
+        // ----- Si el usuario sí existe
+
+        // elimino el usuario
+        repositorio.delete(usuario);
+
+        // HashMap con la respuesta de la eliminación del usuario
+        Map<String, Boolean> respuesta = new HashMap<>();
+
+        // ("eliminar", true)
+        respuesta.put("eliminar", Boolean.TRUE);
+
+        return ResponseEntity.ok(respuesta);
     }
 }
