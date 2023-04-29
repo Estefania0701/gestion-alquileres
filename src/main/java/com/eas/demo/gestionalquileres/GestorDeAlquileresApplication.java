1240607eaf420d4ef6bb82dbbfe671379b990bb1
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-28 21:28:59 -0500

    Initial commit

---
+++ src/main/java/com/eas/demo/gestionalquileres/GestorDeAlquileresApplication.java
@@ -0,0 +1,13 @@
+package com.eas.demo.gestionalquileres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeAlquileresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeAlquileresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eas/demo/gestionalquileres/GestorDeAlquileresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eas.demo.gestionalquileres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeAlquileresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
