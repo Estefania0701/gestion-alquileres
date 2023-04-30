4443d2c5cdce66cfbbf189b96bf34c4f425f8dd8
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-30 15:48:54 -0500

    Elimino anotación innecesaria

--- src/main/java/com/eas/demo/gestionalquileres/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/eas/demo/gestionalquileres/repositorio/UsuarioRepositorio.java
@@ -3,7 +3,6 @@ package com.eas.demo.gestionalquileres.repositorio;
 import com.eas.demo.gestionalquileres.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
