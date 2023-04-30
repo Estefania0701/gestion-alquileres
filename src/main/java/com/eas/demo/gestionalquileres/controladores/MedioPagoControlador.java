4fed6d89c40fb8cab053b10a7dfd65a1fce5c417
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-30 15:46:42 -0500

    Creo el controlador de la clase MedioPago

---
+++ src/main/java/com/eas/demo/gestionalquileres/controladores/MedioPagoControlador.java
@@ -0,0 +1,25 @@
+package com.eas.demo.gestionalquileres.controladores;
+
+import com.eas.demo.gestionalquileres.modelos.MedioPago;
+import com.eas.demo.gestionalquileres.repositorio.MedioPagoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
+public class MedioPagoControlador {
+
+    @Autowired
+    private MedioPagoRepositorio repositorio;
+
+    @GetMapping("/mediospago")
+    public List<MedioPago> consultarMediosPago() {
+        return repositorio.findAll();
+    }
+}
