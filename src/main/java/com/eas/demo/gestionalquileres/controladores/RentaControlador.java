2e7891fa2e95d61f167faa53c90cf363b0eb90dd
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-05-01 19:53:28 -0500

    Agrego método GET por id de renta

--- src/main/java/com/eas/demo/gestionalquileres/controladores/RentaControlador.java
+++ src/main/java/com/eas/demo/gestionalquileres/controladores/RentaControlador.java
@@ -1,15 +1,18 @@
 package com.eas.demo.gestionalquileres.controladores;
 
+import com.eas.demo.gestionalquileres.excepciones.ResourceNotFoundException;
 import com.eas.demo.gestionalquileres.modelos.Renta;
 import com.eas.demo.gestionalquileres.repositorio.RentaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200/")
+//@CrossOrigin(origins = "https://estefania0701.github.io/")
 public class RentaControlador {
 
     @Autowired
@@ -27,5 +30,26 @@ public class RentaControlador {
         return repositorio.save(renta);
     }
 
+    // Obtiene una renta por su ID
+    @GetMapping("/rentas/{id}")
+    public ResponseEntity<Renta> consultarRentaPorId(@PathVariable Long id) {
+        Renta renta = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe la renta con el ID: " + id));
+        return ResponseEntity.ok(renta);
+    }
 
+    // Actualiza una renta
+    @PutMapping("/rentas/{id}")
+    public ResponseEntity<Renta> actualizarRenta (@PathVariable Long id, @RequestBody Renta detallesRenta) {
+        Renta renta = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe la renta con el ID: " + id));
+
+        renta.setEstado(detallesRenta.getEstado());
+        renta.setInicio(detallesRenta.getInicio());
+        renta.setFin(detallesRenta.getFin());
+
+        Renta rentaActualizada = repositorio.save(renta);
+
+        return ResponseEntity.ok(rentaActualizada);
+    }
 }
