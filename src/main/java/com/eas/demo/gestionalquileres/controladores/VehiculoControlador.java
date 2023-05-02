72ea1e86aa920923be5a0e5d7b757a44fd2178d3
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-05-01 19:54:35 -0500

    Agrego métodos GET y PUT

--- src/main/java/com/eas/demo/gestionalquileres/controladores/VehiculoControlador.java
+++ src/main/java/com/eas/demo/gestionalquileres/controladores/VehiculoControlador.java
@@ -1,18 +1,18 @@
 package com.eas.demo.gestionalquileres.controladores;
 
+import com.eas.demo.gestionalquileres.excepciones.ResourceNotFoundException;
 import com.eas.demo.gestionalquileres.modelos.Vehiculo;
 import com.eas.demo.gestionalquileres.repositorio.VehiculoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200/")
+//@CrossOrigin(origins = "https://estefania0701.github.io/")
 public class VehiculoControlador {
 
     @Autowired
@@ -22,5 +22,26 @@ public class VehiculoControlador {
     @GetMapping("/vehiculos")
     public List<Vehiculo> consultarVehiculos() {
         return repositorio.findAll();
+    }
+
+    // Obtiene un vehículo por su id
+    @GetMapping("/vehiculos/{id}")
+    public ResponseEntity<Vehiculo> consultarVehiculoPorId(@PathVariable Long id) {
+        Vehiculo vehiculo = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el vehículo con el ID: " + id));
+        return ResponseEntity.ok(vehiculo);
+    }
+
+    // Actualiza el estado del vehículo (rentado o disponible)
+    @PutMapping("/vehiculos/{id}")
+    public ResponseEntity<Vehiculo> actualizarEstadoVehiculo(@PathVariable Long id, @RequestBody Vehiculo detallesVehiculo) {
+        Vehiculo vehiculo = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el vehículo con el ID: " + id));
+
+        vehiculo.setEstado((detallesVehiculo.getEstado()));
+
+        Vehiculo vehiculoActuwalizado = repositorio.save(vehiculo);
+
+        return ResponseEntity.ok(vehiculoActuwalizado);
     }
 }
